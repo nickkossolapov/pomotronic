dbf5ded92a93dbaefec9c9b60cd55b6460da2a97
nickkossolapov <nickkossolapov@gmail.com>
2025-07-06 16:33:33 +0100

    Add title bar

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import './App.css';
-import Titlebar from "./components/Titlebar.tsx";
+import { useState } from 'react';
+import './App.module.css';
+import Titlebar from './components/Titlebar';
 
-const App: React.FC = () => {
+function App(){
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleToggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const handleMinimize = () => {
+    // TODO: Implement IPC communication with main process
+    console.log('Minimize window');
+  };
+
+  const handleClose = () => {
+    // TODO: Implement IPC communication with main process
+    console.log('Close window');
+  };
+
   return (
     <div id="app">
-      <Titlebar />
-      {/* Other components will be added here as they're migrated */}
+      <Titlebar
+        drawerOpen={drawerOpen}
+        onToggleDrawer={handleToggleDrawer}
+        onMinimize={handleMinimize}
+        onClose={handleClose}
+      />
     </div>
   );
-};
+}
 
 export default App;
--- src/renderer/components/Titlebar.tsx
+++ src/renderer/components/Titlebar.tsx
@@ -1,129 +1,110 @@
-import React from 'react';
-import { useViewStore } from '../stores';
 import styles from './Titlebar.module.css';
 
-// Electron IPC will be available in the renderer process
-declare global {
-  interface Window {
-    electron?: {
-      ipcRenderer: {
-        send: (channel: string, ...args: unknown[]) => void;
-      };
-    };
-  }
+interface TitlebarProps {
+  drawerOpen: boolean;
+  onToggleDrawer: () => void;
+  onMinimize: () => void;
+  onClose: () => void;
 }
 
-const Titlebar: React.FC = () => {
-  const drawerOpen = useViewStore((state) => state.drawerOpen);
-  const toggleDrawer = useViewStore((state) => state.toggleDrawer);
-
-  const handleMinimize = () => {
-    if (window.electron?.ipcRenderer) {
-      window.electron.ipcRenderer.send('win-minimize');
-    }
-  };
-
-  const handleClose = () => {
-    if (window.electron?.ipcRenderer) {
-      window.electron.ipcRenderer.send('win-close');
-    }
-  };
-
-  const MinimizeIcon: React.FC = () => (
-    <svg
-      version="1.2"
-      baseProfile="tiny"
-      xmlns="http://www.w3.org/2000/svg"
-      x="0px"
-      y="0px"
-      viewBox="0 0 14 2"
-      xmlSpace="preserve"
-      className={styles.iconMinimize}
-    >
-      <line
-        fill="none"
-        stroke="#F6F2EB"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeMiterlimit="10"
-        x1="1"
-        y1="1"
-        x2="13"
-        y2="1"
-      />
-    </svg>
-  );
-
-  const CloseIcon: React.FC = () => (
-    <svg
-      version="1.2"
-      baseProfile="tiny"
-      xmlns="http://www.w3.org/2000/svg"
-      x="0px"
-      y="0px"
-      viewBox="0 0 12.6 12.6"
-      xmlSpace="preserve"
-      className={styles.iconClose}
-    >
-      <line
-        fill="none"
-        stroke="#F6F2EB"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeMiterlimit="10"
-        x1="1"
-        y1="1"
-        x2="11.6"
-        y2="11.6"
-      />
-      <line
-        fill="none"
-        stroke="#F6F2EB"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeMiterlimit="10"
-        x1="11.6"
-        y1="1"
-        x2="1"
-        y2="11.6"
-      />
-    </svg>
-  );
-
+function Titlebar({ drawerOpen, onToggleDrawer, onMinimize, onClose }: TitlebarProps) {
   return (
     <nav className={styles.titlebar}>
-      {/* Menu toggle button */}
+      {/* menu */}
       <div
-        className={styles.iconWrapperSingle}
+        className={`${styles.iconWrapper} ${styles.iconWrapperTitlebar} ${styles.iconWrapperSingle}`}
+        style={{ position: 'absolute' }}
         title="Settings"
-        onClick={toggleDrawer}
+        onClick={onToggleDrawer}
       >
-        <div className={`${styles.menuWrapper} ${drawerOpen ? styles.collapsed : ''}`}>
+        <div className={`${styles.menuWrapper} ${drawerOpen ? styles.isCollapsed : ''}`}>
           <div className={styles.menuLine}></div>
           <div className={styles.menuLine}></div>
         </div>
       </div>
 
-      {/* App title */}
       <h1 className={styles.title}>Pomotroid</h1>
 
-      {/* Window controls */}
-      <div className={styles.iconGroup}>
+      <div className={styles.iconGroup} style={{ position: 'absolute', top: 0, right: 0 }}>
         <div
-          className={styles.iconWrapperDoubleLeft}
-          onClick={handleMinimize}
+          className={`${styles.iconWrapper} ${styles.iconWrapperTitlebar} ${styles.iconWrapperDoubleLeft}`}
+          style={{ paddingLeft: '18px' }}
+          onClick={onMinimize}
         >
-          <MinimizeIcon />
+          {/* minimize */}
+          <svg
+            version="1.2"
+            baseProfile="tiny"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 14 2"
+            xmlSpace="preserve"
+            width="15px"
+            height="20px"
+            className={`${styles.icon} ${styles.iconMinimize}`}
+          >
+            <line
+              fill="none"
+              stroke="#F6F2EB"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              x1="1"
+              y1="1"
+              x2="13"
+              y2="1"
+            />
+          </svg>
         </div>
         <div
-          className={styles.iconWrapperDoubleRight}
-          onClick={handleClose}
+          className={`${styles.iconWrapper} ${styles.iconWrapperTitlebar} ${styles.iconWrapperDoubleRight}`}
+          style={{ paddingRight: '18px' }}
+          onClick={onClose}
         >
-          <CloseIcon />
+          {/* close */}
+          <svg
+            version="1.2"
+            baseProfile="tiny"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 12.6 12.6"
+            xmlSpace="preserve"
+            height="15px"
+            className={`${styles.icon} ${styles.iconClose}`}
+          >
+            <line
+              fill="none"
+              stroke="#F6F2EB"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              x1="1"
+              y1="1"
+              x2="11.6"
+              y2="11.6"
+            />
+            <line
+              fill="none"
+              stroke="#F6F2EB"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              x1="11.6"
+              y1="1"
+              x2="1"
+              y2="11.6"
+            />
+          </svg>
         </div>
       </div>
     </nav>
   );
-};
+}
 
 export default Titlebar;
